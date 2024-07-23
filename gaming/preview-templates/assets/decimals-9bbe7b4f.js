5c4fb7a2d9607f59f8e63c156da0fdf77f621c8d
CatKevin <catmi9944@gmail.com>
2024-07-23 10:38:25 +0800

    upd(gaming): update gaming logic and UI

--- gaming/preview-templates/assets/decimals-9bbe7b4f.js
+++
@@ -1,1 +0,0 @@
-import{bJ as a}from"./index-cc3bb452.js";import{d as e}from"./decimals-2074cfe1.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
