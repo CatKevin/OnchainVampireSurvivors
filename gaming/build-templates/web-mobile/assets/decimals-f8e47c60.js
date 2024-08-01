58b11370cad17c3476d54b22ca94cd682847a8b7
CatKevin <catmi9944@gmail.com>
2024-08-01 13:41:42 +0800

    feat(gaming, contract, thirdweb): Implement all game logic

---
+++ gaming/build-templates/web-mobile/assets/decimals-f8e47c60.js
@@ -0,0 +1,1 @@
+import{bJ as a}from"./index-6d7f0bcb.js";import{d as e}from"./decimals-e281ba2e.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
