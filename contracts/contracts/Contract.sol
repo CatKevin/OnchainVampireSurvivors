d5568ae612a45ad3e1c35ad21fdcd584a5fb094a
CatKevin <catmi9944@gmail.com>
2025-10-27 20:09:22 +0800

    upd(contracts): update ownable func

--- contracts/contracts/Contract.sol
+++ contracts/contracts/Contract.sol
@@ -1,7 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-import "@thirdweb-dev/contracts/extension/Ownable.sol";
+
+interface IOwnable {
+    /// @dev Returns the owner of the contract.
+    function owner() external view returns (address);
+
+    /// @dev Lets a module admin set a new owner for the contract. The new owner must be a module admin.
+    function setOwner(address _newOwner) external;
+
+    /// @dev Emitted when a new Owner is set.
+    event OwnerUpdated(address indexed prevOwner, address indexed newOwner);
+}
+
+abstract contract Ownable is IOwnable {
+    /// @dev The sender is not authorized to perform the action
+    error OwnableUnauthorized();
+
+    /// @dev Owner of the contract (purpose: OpenSea compatibility)
+    address private _owner;
+
+    /// @dev Reverts if caller is not the owner.
+    modifier onlyOwner() {
+        if (msg.sender != _owner) {
+            revert OwnableUnauthorized();
+        }
+        _;
+    }
+
+    /**
+     *  @notice Returns the owner of the contract.
+     */
+    function owner() public view override returns (address) {
+        return _owner;
+    }
+
+    /**
+     *  @notice Lets an authorized wallet set a new owner for the contract.
+     *  @param _newOwner The address to set as the new owner of the contract.
+     */
+    function setOwner(address _newOwner) external override {
+        if (!_canSetOwner()) {
+            revert OwnableUnauthorized();
+        }
+        _setupOwner(_newOwner);
+    }
+
+    /// @dev Lets a contract admin set a new owner for the contract. The new owner must be a contract admin.
+    function _setupOwner(address _newOwner) internal {
+        address _prevOwner = _owner;
+        _owner = _newOwner;
+
+        emit OwnerUpdated(_prevOwner, _newOwner);
+    }
+
+    /// @dev Returns whether owner can be set in the given execution context.
+    function _canSetOwner() internal view virtual returns (bool);
+}
 
 interface IUltraVerifier {
     function getVerificationKeyHash() external pure returns (bytes32);
